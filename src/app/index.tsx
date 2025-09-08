0b1d6d481efac9109e4e224042122c2d401be75d
PedroHenrick1 <pedrohenrick52@gmail.com>
2025-09-08 19:14:29 -0300

    Inicio_primeira_tela

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,4 +1,4 @@
-import { View, Text, StyleSheet, Alert } from "react-native";
+import { View, Text, StyleSheet, Alert, Image, Platform } from "react-native";
 import { useState } from "react";
 
 import Button from "@/components/button";
@@ -18,22 +18,47 @@ export default function Index() {
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Olá,{name}</Text>
-      <Input onChangeText={setName} />
-      <Button title="Entrar" onPress={handleNext} />
+      <Image
+        source={require("../../assets/images/image_bot.png")}
+        style={styles.imageBot}
+      />
+      <Text style={styles.title}>Olá</Text>
+      <Text style={styles.description}>
+        Irei te ajudar a controlar sua ansiedade ou depressão
+      </Text>
+
+      <Button
+        title="Clique aqui se está precisando
+de ajuda urgente"
+      />
+
+      <Button title="Criar uma conta" onPress={handleNext} />
+
+      <Button title="Fazer login" onPress={handleNext} />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    fontFamily: Platform.OS === "ios" ? "System" : "Roboto",
     flex: 1,
-    padding: 32,
+    padding: 15,
     justifyContent: "center",
     gap: 20,
   },
   title: {
-    color: "red",
-    fontSize: 24,
+    color: "black",
+    fontSize: 50,
+  },
+  description: {
+    flex: 1,
+    alignItems: "center",
+    fontSize: 20,
+  },
+  imageBot: {
+    width: "100%",
+    height: 300,
+    marginLeft: 100,
   },
 });
--- src/components/button/styles.ts
+++ src/components/button/styles.ts
@@ -2,7 +2,7 @@ import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
   button: {
-    width: "100%",
+    width: 300,
     height: 52,
     backgroundColor: "blue",
     justifyContent: "center",
